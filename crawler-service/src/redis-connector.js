b407db75af615eb1d9327d70e17780a0e3800919
Alan Cao <alannesta@sina.com>
2017-04-07 16:27:03 -0400

    redis image

--- crawler-service/src/redis-connector.js
+++ crawler-service/src/redis-connector.js
@@ -1,7 +1,7 @@
 const redis = require("redis");
 
 // TODO: configuration with env variables
-const client = redis.createClient();
+const client = redis.createClient('//127.0.0.1:6379');
 
 module.exports = client;
 // let msg_count = 0;
@@ -12,7 +12,7 @@ module.exports = client;
 // client.hgetall("pool-test", function (err, obj) {
 // 	console.dir(obj);
 // });
-
+//
 // client.on("message", function (channel, message) {
 // 	console.log("sub channel " + channel + ": " + message);
 // 	msg_count += 1;
